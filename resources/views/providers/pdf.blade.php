36c997a116b9e9769198c1af1e18e22961529d1e
VictorSanchezS <sshapiamavictor@crece.uss.edu.pe>
2023-12-13 18:35:19 -0500

    Project finished

--- resources/views/providers/pdf.blade.php
+++ resources/views/providers/pdf.blade.php
@@ -1,47 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>List of Providers</title>
     <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            margin: 20px;
+        * {
+            /* Change your font family */
+            font-family: sans-serif;
         }
-
-        h1 {
+        h1{
             text-align: center;
-            color: #333;
+            color: #009879;
         }
 
-        table {
-            width: 100%;
+        .content-table {
             border-collapse: collapse;
-            margin-top: 20px;
+            margin: 25px 0;
+            font-size: 0.9em;
+            min-width: 400px;
+            border-radius: 5px 5px 0 0;
+            overflow: hidden;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
         }
 
-        th, td {
-            padding: 12px;
+        .content-table thead tr {
+            background-color: #009879;
+            color: #ffffff;
             text-align: left;
-            border-bottom: 1px solid #ddd;
+            font-weight: bold;
         }
 
-        th {
-            background-color: #f2f2f2;
+        .content-table th,
+        .content-table td {
+            padding: 12px 15px;
         }
 
-        tr:hover {
-            background-color: #f5f5f5;
+        .content-table tbody tr {
+            border-bottom: 1px solid #dddddd;
         }
+
+        .content-table tbody tr:nth-of-type(even) {
+            background-color: #f3f3f3;
+        }
+
+        .content-table tbody tr:last-of-type {
+            border-bottom: 2px solid #009879;
+        }
+
     </style>
 </head>
+
 <body>
     <h1>List of Providers</h1>
-    <table border="1">
+    <table class="content-table">
         <thead>
             <tr>
+                <th>#</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Phone</th>
@@ -53,6 +70,7 @@
         <tbody>
             @foreach ($providers as $provider)
                 <tr>
+                    <th>{{ $loop->iteration }}</th>
                     <td>{{ $provider['name'] }}</td>
                     <td>{{ $provider['email'] }}</td>
                     <td>{{ $provider['phone'] }}</td>
@@ -64,4 +82,5 @@
         </tbody>
     </table>
 </body>
+
 </html>
